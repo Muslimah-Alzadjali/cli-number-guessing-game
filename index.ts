9fab4c02050a416e3eabe4845049bbe02015c23b
Muslimah-Alzadjali <165010671+Muslimah-Alzadjali@users.noreply.github.com>
2024-03-28 13:05:24 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("CLI Number Guessing Game");
+
+const randomNumber = Math.floor(Math.random()* 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+
+if (answer.userGuessedNumber === randomNumber){
+    console.log("Congratulations ! You guess a correct number");
+}
+    else{
+        console.log("Sorry, you guess a wrong number");
+    }
+    
